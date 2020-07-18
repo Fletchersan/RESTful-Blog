8f697c25d1a4d174b30b1294e11553b87af3619f
Amogh Dhar Diwan <amoghdhardiwan@gmail.com>
2020-07-18 19:06:34 +0530

    Done with routing, next sanitization code

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
 //configure the app
 app.set('view engine', 'ejs');
 app.use(express.static('public'));  //use to serve custom stylesheets
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 //Configure Mongoose
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 //Setup schema and model/ MODEL CONFIG  
@@ -49,6 +51,27 @@ app.get("/blogs/:id", (req, res) => {
     .then( blog => res.render('show', {blog: blog}))
     .catch(err => res.redirect('/blogs'));
 });
+
+//Edit 
+app.get("/blogs/:id/edit", (req, res) => {
+    Blog.findById(req.params.id)
+    .then(blog => res.render("edit", {blog: blog}))
+    .catch( err => res.redirect("/blogs"));
+})
+// Update
+app.put("/blogs/:id", (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog)
+    .then(() => res.redirect(`/blogs/${req.params/id}`))
+    .catch( () => res.redirect('/blogs'));
+    // res.send("update Route")
+})
+
+//Delete
+app.delete("/blogs/:id", (req, res) => {
+    Blog.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect('/blogs'))
+    .catch(() => res.redirect('/blogs'));
+})
 
 app.listen(3000, () => {
     console.log("server running at localhost:3000");
